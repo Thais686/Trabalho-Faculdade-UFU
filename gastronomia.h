4119eda49c67886fed64c0d233d41aa854d0e159
Thaís <thaiscastro95@yahoo.com>
2026-08-03 19:11:49 -0300

    Refactor struct definitions in gastronomia.h

--- gastronomia.h
+++ gastronomia.h
@@ -1,47 +1,47 @@
 #ifndef _GASTRONOMIA_H
 #define _GASTRONOMIA_H
 
-struct prato{
-    
-    int id;
-    char nome[500];
-    char ingredientes[700];
-    char modoPreparo[700];
-    float tempoPreparo;
-    
-   struct prato *ant;
-   struct prato *prox;
+struct prato {
+
+	int id;
+	char nome[500];
+	char ingredientes[700];
+	char modoPreparo[700];
+	float tempoPreparo;
+
+	struct prato *ant;
+	struct prato *prox;
 };
 typedef struct prato Prato;
 
-struct listaPrato{
-    
-    Prato *inicio;
-    Prato *fim;
-    int qtd;
+struct listaPrato {
+
+	Prato *inicio;
+	Prato *fim;
+	int qtd;
 };
 
 typedef struct listaPrato ListaPrato;
 
 
-struct regiao{
-    
-    int id;
-    char nome[100];
-    char descricao[400];
-    
-    ListaPrato pratos;
-    
-    struct regiao *ant;
-    struct regiao *prox;
+struct regiao {
+
+	int id;
+	char nome[100];
+	char descricao[400];
+
+	ListaPrato pratos;
+
+	struct regiao *ant;
+	struct regiao *prox;
 };
 typedef struct regiao Regiao;
 
-struct listaRegiao{
-    
-    Regiao *inicio;
-    Regiao *fim;
-    int qtd;
+struct listaRegiao {
+
+	Regiao *inicio;
+	Regiao *fim;
+	int qtd;
 };
 
 typedef struct listaRegiao ListaRegiao;
@@ -53,8 +53,8 @@ void carregarPratos(ListaRegiao *lr);
 ListaRegiao *criarRegiao();
 int inserirRegiao(ListaRegiao *lr, int id, char nome[], char descricao[]);
 Regiao *buscarElementoRegiao(ListaRegiao *lr,int id);
-void alterarRegiao(ListaRegiao *lr, int id);
-void removerRegiao(ListaRegiao *lr,int id);
+int alterarRegiao(ListaRegiao *lr, int id);
+int removerRegiao(ListaRegiao *lr,int id);
 void listarRegiao(ListaRegiao *lr);
 int qtdRegiao(ListaRegiao *lr);
 
@@ -63,8 +63,8 @@ int qtdRegiao(ListaRegiao *lr);
 ListaPrato *criarPrato();
 int inserirPrato(ListaPrato *lp, int id, char nome[], char ingredientes[],char modoPreparo[],float tempoPreparo);
 Prato *buscarElementoPrato(ListaPrato *lp,int id);
-void alterarPrato(ListaPrato *lp, int id);
-void removerPrato(ListaPrato *lp,int id);
+int alterarPrato(ListaPrato *lp, int id);
+int removerPrato(ListaPrato *lp,int id);
 void listarPrato(ListaPrato *lp);
 int qtdPrato(ListaPrato *lp);
 
